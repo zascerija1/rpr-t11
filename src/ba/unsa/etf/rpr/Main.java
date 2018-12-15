08cd48df8cbd4f421dfc9dc2c969b33479576c23
Vedran Ljubović <vljubovic@smartnet.ba>
2018-12-15 19:42:04 +0100

    Initial commit

---
+++ src/ba/unsa/etf/rpr/Main.java
@@ -0,0 +1,12 @@
+package ba.unsa.etf.rpr;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Gradovi su:\n" + ispisiGradove());
+        glavniGrad();
+    }
+}
